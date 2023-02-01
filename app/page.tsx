51ec1f57c6bfead1d24a7e28851d173fc4417fe7
1qjc <102451300+1qjc@users.noreply.github.com>
2023-02-01 16:01:48 +0800

    init

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div>
+      <h1 className="text-4xl">Page</h1>
+      <p>Some Content</p>
+    </div>
+  );
+}
+
+export default HomePage;
