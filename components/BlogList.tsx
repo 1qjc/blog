75467c675f708fee1d1e142fead6c79f4b18fb2c
1qjc <102451300+1qjc@users.noreply.github.com>
2023-02-05 11:01:20 +0800

    add tailwind line clamp plugin

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -1,6 +1,6 @@
+import { ArrowUpRightIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
 import urlFor from "../lib/urlFor";
-import category from "../schemas/category";
 
 type Props = {
   posts: Post[];
@@ -13,7 +13,7 @@ function BlogList({ posts }: Props) {
       <div className="grid grid-cols-1 md:grid-cols-2 px-10 gap-10 gap-y-16 pb-24">
         {posts.map((post) => (
           <div key={post._id} className="flex flex-col group cursor-pointer">
-            <div className="relative w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out">
+            <div className="relative rounded-lg overflow-hidden w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out ">
               <Image
                 className="object-cover object-left lg:object-center"
                 src={urlFor(post.mainImage).url()}
@@ -33,7 +33,9 @@ function BlogList({ posts }: Props) {
                 </div>
                 <div className="flex flex-col md:flex-row gap-y-2 md:gap-x-2 items-center">
                   {post.categories?.map((category) => (
-                    <div className="bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold">
+                    <div
+                      key={category._id}
+                      className="bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold">
                       <p>{category.title}</p>
                     </div>
                   ))}
@@ -43,8 +45,15 @@ function BlogList({ posts }: Props) {
 
             <div className="mt-5 flex-1">
               <p className="underline text-lg font-bold">{post.title}</p>
-              <p className="line-clamp-2 text-gray-500">{post.description}</p>
+              <p className="line-clamp-2 text-gray-500">
+                {post.body![0].children[0].text}
+              </p>
             </div>
+
+            <p className="mt-5 font-bold flex items-center group-hover:underline">
+              Read Post
+              <ArrowUpRightIcon className="ml-2 h-4 w-4" />
+            </p>
           </div>
         ))}
       </div>
